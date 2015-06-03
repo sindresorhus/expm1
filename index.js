fd527add50fb09cc09aa63424df29660bc5a5f23
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-06-03 21:07:35 +0200

    Simplify implementation
    Ref https://github.com/sindresorhus/expm1/commit/c21d8d47473767763c3cd32b859b44ee0f794022#commitcomment-11503563

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 'use strict';
-var isFinite = require('is-finite');
-
 module.exports = Math.expm1 || function (x) {
-	if (x === -Infinity) {
-		return -1;
-	}
-
-	if (!isFinite(x) || x === 0) {
-		return x;
-	}
-
 	return Math.exp(x) - 1;
 };
