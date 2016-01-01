676331a6d18bc1e18c67a150976b79d268339af3
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 14:03:24 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,15 +1,14 @@
-'use strict';
-var test = require('ava');
-Math.expm1 = undefined;
-var expm1 = require('./');
+import test from 'ava';
 
-test(function (t) {
-	t.assert(expm1(0) === 0);
-	t.assert(expm1(-1) === -0.6321205588285577);
-	t.assert(expm1(1) === 1.718281828459045);
-	t.assert(expm1(0) === 0);
-	t.assert(expm1(-0) === -0);
-	t.assert(expm1(Infinity) === Infinity);
-	t.assert(expm1(-Infinity) === -1);
-	t.end();
+Math.expm1 = undefined;
+const m = require('./');
+
+test(t => {
+	t.is(m(0), 0);
+	t.is(m(-1), -0.6321205588285577);
+	t.is(m(1), 1.718281828459045);
+	t.is(m(0), 0);
+	t.is(m(-0), -0);
+	t.is(m(Infinity), Infinity);
+	t.is(m(-Infinity), -1);
 });
